aedea61638dd89cf9a6356c41e4595040c6b3eb3
alikurtulush92 <kopanarya92@gmail.com>
2020-08-11 12:52:59 +0100

    Updates

---
+++ precache-manifest.a733305717d47b3bbcc07a10a051a595.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aff2fd1b4862f04583164bc3516db59",
+    "url": "/HangmanApp/index.html"
+  },
+  {
+    "revision": "4d8039a32b9f6324cea9",
+    "url": "/HangmanApp/static/css/main.6f7ea0eb.chunk.css"
+  },
+  {
+    "revision": "806b0181a7c678485550",
+    "url": "/HangmanApp/static/js/2.a68efb18.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HangmanApp/static/js/2.a68efb18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d8039a32b9f6324cea9",
+    "url": "/HangmanApp/static/js/main.b6971f58.chunk.js"
+  },
+  {
+    "revision": "575e22d8d03319c08565",
+    "url": "/HangmanApp/static/js/runtime-main.8df137d7.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/HangmanApp/static/media/logo.25bf045c.svg"
+  }
+]);
--- src/choice.js
+++
@@ -1,5 +0,0 @@
-function choice(arr){
-   let randIx = Math.floor(Math.random() * arr.length)
-   return arr[randIx]
-}
-export default choice
